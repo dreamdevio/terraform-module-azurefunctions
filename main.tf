5cb075fd0f42c7c054bfc3d37448913f81260a04
Douglas Aguiar <doaguiar@gmail.com>
2019-12-18 15:34:22 +0000

    adding environment variable to tf module

--- main.tf
+++ main.tf
@@ -1,38 +1,42 @@
 terraform {
     required_version = ">= 0.12"
-    backend "azurerm" {
-        
-    }
+}
+
+variable "environment" {
+    description = "The environment that this deployment will apply to. Such as DEV, QA, PROD."
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group for the microservice"
 }
 
-variable "subscription_id" {
-  description = "The azure subscription id"
-}
-
 provider "azurerm" {
     version         = "1.36.0"
-    subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
-    name     = var.resource_group_name
+    name     = "${var.environment}.${var.resource_group_name}"
     location = "northeurope"
+    
+    tags = {
+        environment = var.environment
+    }
 }
 
 resource "azurerm_storage_account" "sa" {
-    name                     = "${replace(var.resource_group_name, ".", "")}sa"
+    name                     = "${var.environment}${replace(var.resource_group_name, ".", "")}sa"
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
+    
+    tags = {
+        environment = var.environment
+    }
 }
 
 resource "azurerm_app_service_plan" "asp" {
-    name                = "${replace(var.resource_group_name, ".", "-")}sa"
+    name                = "${var.environment}-${replace(var.resource_group_name, ".", "-")}sa"
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     kind                = "FunctionApp"
@@ -41,12 +45,20 @@ resource "azurerm_app_service_plan" "asp" {
         tier = "Dynamic"
         size = "Y1"
     }
+    
+    tags = {
+        environment = var.environment
+    }
 }
 
 resource "azurerm_function_app" "fa" {
-    name                      = "${replace(var.resource_group_name, ".", "-")}app"
+    name                      = "${var.environment}-${replace(var.resource_group_name, ".", "-")}app"
     location                  = azurerm_resource_group.rg.location
     resource_group_name       = azurerm_resource_group.rg.name
     app_service_plan_id       = azurerm_app_service_plan.asp.id
     storage_connection_string = azurerm_storage_account.sa.primary_connection_string
+    
+    tags = {
+        environment = var.environment
+    }
 }
