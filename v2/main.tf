16abede58b4139cd2b4527879333470a74bdfea2
douglasaguiar <doaguiar@gmail.com>
2020-04-22 10:44:21 -0300

    testing another way to set the block

--- v2/main.tf
+++ v2/main.tf
@@ -90,7 +90,9 @@ resource "azurerm_function_app" "func" {
     storage_connection_string = azurerm_storage_account.st.primary_connection_string
     version = "~3"
 
-    auth_settings = var.extra_function_app_configs.auth_settings
+    auth_settings {
+        var.extra_function_app_configs.auth_settings
+    }
 
     tags = local.default_tags
 }
